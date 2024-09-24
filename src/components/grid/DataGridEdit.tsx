759a91293beeb166294cf5865cb953c5f6be92f9
Le Trung Kien <kienlt@smartosc.com>
2024-09-24 18:53:43 +0700

    select image for editor

--- src/components/grid/DataGridEdit.tsx
+++ src/components/grid/DataGridEdit.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import axios from "@/api/axiosInstance";
 import { buildTree, flatTree } from "@/api/tree";
@@ -61,6 +61,7 @@ interface DataGridEditProps {
 }
 
 const DataGridEdit: React.FC<DataGridEditProps> = ({ mode, table, itemId, addNewLabel, updateLabel, fields, item, setItem, handleUpdateItem, handleCancelEdit, handleAddItem, handleCancelAdd }): React.ReactElement => {
+    const [selectedImage, setSelectedImage] = useState<string>("");
 
     useEffect(() => {
         const setMapsForFields = () => {
@@ -285,47 +286,61 @@ const DataGridEdit: React.FC<DataGridEditProps> = ({ mode, table, itemId, addNew
     }
 
     const FieldEditorRenderer = (field: DataGridEditField, item: any) => {
+        const editorRef = useRef<any>(null); // Initialize the editorRef
+        const handleImageInsert = (imagePath: string) => {
+            if (editorRef.current) {
+                editorRef.current.insertContent(`<img src="http://localhost:3002/uploads${imagePath}" alt="Selected Image"/>`);
+            }
+            setSelectedImage(imagePath); // Save selected image path
+        };
         return (
-            <Editor
-                tinymceScriptSrc="/tinymce/tinymce.min.js"
-                initialValue={item[field.index]}
-                init={{
-                    height: 400,
-                    plugins: [
-                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
-                        'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                        'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-                    ],
-                    toolbar:
-                        'undo redo | formatselect | ' +
-                        'bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | ' +
-                        'bullist numlist outdent indent | ' +
-                        'link image media charmap anchor | insertdatetime table | ' +
-                        'visualblocks code preview fullscreen | ' +
-                        'searchreplace | help | wordcount',
+            <>
+                <Editor
+                    tinymceScriptSrc="/tinymce/tinymce.min.js"
+                    initialValue={item[field.index]}
+                    onInit={(evt, editor) => (editorRef.current = editor)} // Store the editor instance in the ref
+                    init={{
+                        height: 400,
+                        plugins: [
+                            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
+                            'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                        ],
+                        toolbar:
+                            'undo redo | formatselect | ' +
+                            'bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | ' +
+                            'bullist numlist outdent indent | ' +
+                            'link image media charmap anchor | insertdatetime table | ' +
+                            'visualblocks code preview fullscreen | ' +
+                            'searchreplace | help | wordcount',
 
-                    toolbar_sticky: true, // Sticky toolbar
-                    menubar: 'file edit view insert format tools table help', // Menubar options
+                        toolbar_sticky: true, // Sticky toolbar
+                        menubar: 'file edit view insert format tools table help', // Menubar options
 
-                    // Provide the path to your local TinyMCE installation
-                    script_url: '/tinymce/tinymce.min.js',
-                    external_plugins: {
-                        advlist: '/tinymce/plugins/advlist/plugin.min.js',
-                        autolink: '/tinymce/plugins/autolink/plugin.min.js',
-                        lists: '/tinymce/plugins/lists/plugin.min.js',
-                        link: '/tinymce/plugins/link/plugin.min.js',
-                        image: '/tinymce/plugins/image/plugin.min.js',
-                        charmap: '/tinymce/plugins/charmap/plugin.min.js',
-                        preview: '/tinymce/plugins/preview/plugin.min.js',
-                        anchor: '/tinymce/plugins/anchor/plugin.min.js',
-                    },
-                }}
-                onEditorChange={(content) => {
-                    let updatedItem = { ...item };
-                    updatedItem[field.index] = content;
-                    setItem(updatedItem);
-                }}
-            />
+                        // Provide the path to your local TinyMCE installation
+                        script_url: '/tinymce/tinymce.min.js',
+                        external_plugins: {
+                            advlist: '/tinymce/plugins/advlist/plugin.min.js',
+                            autolink: '/tinymce/plugins/autolink/plugin.min.js',
+                            lists: '/tinymce/plugins/lists/plugin.min.js',
+                            link: '/tinymce/plugins/link/plugin.min.js',
+                            image: '/tinymce/plugins/image/plugin.min.js',
+                            charmap: '/tinymce/plugins/charmap/plugin.min.js',
+                            preview: '/tinymce/plugins/preview/plugin.min.js',
+                            anchor: '/tinymce/plugins/anchor/plugin.min.js',
+                        },
+                    }}
+                    onEditorChange={(content) => {
+                        let updatedItem = { ...item };
+                        updatedItem[field.index] = content;
+                        setItem(updatedItem);
+                    }}
+                />
+                <ImageSelector
+                    selectedImage={selectedImage}
+                    setSelectedImage={handleImageInsert}
+                />
+            </>
         );
     };
 
@@ -340,7 +355,7 @@ const DataGridEdit: React.FC<DataGridEditProps> = ({ mode, table, itemId, addNew
                 }}
             />
         );
-    };    
+    };
 
     const FieldUndefinedRenderer = (field: DataGridEditField, item: any) => {
         return '-';
