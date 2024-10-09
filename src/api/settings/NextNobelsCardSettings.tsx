00d4eba9a6f5a1874a4a79f9b5845a208aa3f74c
Le Trung Kien <kienlt@smartosc.com>
2024-10-09 16:50:06 +0700

    settings

--- src/api/settings/NextNobelsCardSettings.tsx
+++ src/api/settings/NextNobelsCardSettings.tsx
@@ -1,107 +1,47 @@
-/**
- *
- * -- nextnobels.card_nextnobels definition
-
-CREATE TABLE `card_nextnobels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pincard` varchar(255) NOT NULL,
-  `pincard_normal` varchar(255) NOT NULL,
-  `price` int(11) NOT NULL,
-  `discount` int(11) NOT NULL,
-  `serial` varchar(255) NOT NULL,
-  `creatorId` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modifiedId` int(11) NOT NULL,
-  `modified` datetime NOT NULL,
-  `activedId` int(11) NOT NULL,
-  `actived` datetime NOT NULL,
-  `status` int(11) NOT NULL,
-  `languages` varchar(255) DEFAULT NULL,
-  `time` int(11) NOT NULL,
-  `class` tinyint(4) DEFAULT NULL,
-  `software` int(11) DEFAULT NULL,
-  `site` int(11) DEFAULT NULL,
-  `promotion` tinyint(4) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `expiredDate` datetime NOT NULL,
-  `startDate` datetime NOT NULL,
-  `endDate` datetime NOT NULL,
-  `couponable` tinyint(4) DEFAULT NULL,
-  `resellerId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9124 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
- */
-import { DataGridColumn, DataGridColumnActionType, DataGridColumnType, DataGridFilterColumn, DataGridFilterColumnType, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridTableJoin } from "@/components/grid/DataGrid";
+import { DataGridColumn, DataGridColumnActionType, DataGridColumnType, DataGridColumns, DataGridFilterColumn, DataGridFilterColumnType, DataGridFilterColumns, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridSortOptions, DataGridTableJoin } from "@/components/grid/DataGrid";
 import { DataGridEditField, DataGridEditFieldType } from "@/components/grid/DataGridEdit";
 import { TableGridSettings } from "@/components/grid/TableGrid";
 
 const gridTitle: string = "Quản lý Thẻ cào NextNobels";
+const gridAddNewLabel: string = "Thêm mới Thẻ cào NextNobels";
+const gridUpdateLabel: string = "Cập nhật Thẻ cào NextNobels";
+const gridDeleteSelectedsLabel: string = "Xóa các Thẻ cào NextNobels đã chọn";
 const gridTable: string = "card_nextnobels";
 const gridJoins: DataGridTableJoin[] = [];
 const gridSearchFields: string[] = ["id", "pincard", "pincard_normal", "serial"];
 const gridFields: string[] = ["id", "pincard", "pincard_normal", "price", "serial", "actived", "status", "languages", "time", "class", "promotion", "quantity", "expiredDate", "startDate", "endDate", "couponable"];
 
 const gridColumns: DataGridColumn[] = [
-    { index: "id", label: "ID", width: "1%" },
+    DataGridColumns.id,
     { index: "pincard", label: "Mã thẻ" },
     { index: "pincard_normal", label: "Mã thẻ thông thường" },
     { index: "price", label: "Giá", type: DataGridColumnType.CURRENCY },
     { index: "serial", label: "Số serial" },
-    { index: "actived", label: "Ngày kích hoạt", type: DataGridColumnType.DATE},
+    { index: "actived", label: "Ngày kích hoạt", type: DataGridColumnType.DATE },
     { index: "languages", label: "Ngôn ngữ" },
     { index: "time", label: "Thời gian", type: DataGridColumnType.NUMBER },
     { index: "class", label: "Lớp" },
     { index: "promotion", label: "Khuyến mãi", type: DataGridColumnType.STATUS, statusToggable: true, hideLabel: true },
     { index: "quantity", label: "Số lượng" },
     { index: "couponable", label: "Có thể áp dụng coupon", type: DataGridColumnType.STATUS, statusToggable: true, hideLabel: true },
-    {
-        index: "status", type: DataGridColumnType.STATUS, label: "Trạng thái", map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        hideLabel: true,
-        statusToggable: true
-    },
-    { index: "editAction", label: "Sửa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.EDIT, width: "1%" },
-    { index: "deleteAction", label: "Xóa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.DELETE, width: "1%" }
+    DataGridColumns.status,
+    DataGridColumns.editAction,
+    DataGridColumns.deleteAction,
 ];
 
-const gridPagination: DataGridPagination = {
-    currentPage: 1,
-    pageSize: 50
-};
+const gridPagination: DataGridPagination = { currentPage: 1, pageSize: 50 };
 
 const gridFilters: DataGridFilterColumn[] = [
-    { index: "id", label: "ID", type: DataGridFilterColumnType.TEXT },
-    {
-        index: "status", label: "Trạng thái", type: DataGridFilterColumnType.STATUS, map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        comparisonOperator: "equal"
-    },
+    DataGridFilterColumns.id,
+    DataGridFilterColumns.status,
 ];
 
 const gridSortOptions: DataGridSortOption[] = [
-    {
-        index: "idAsc",
-        label: "ID tăng",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.DESCENDING },
-        ]
-    },
-    {
-        index: "idDesc",
-        label: "ID giảm",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.ASCENDING },
-        ]
-    }
+    DataGridSortOptions.idAsc,
+    DataGridSortOptions.idDesc,
 ];
 
-const gridDefaultSorts: DataGridSort[] = [
-    { index: "id", direction: DataGridSortDirection.DESCENDING }
-];
+const gridDefaultSorts: DataGridSort[] = [{ index: "id", direction: DataGridSortDirection.DESCENDING }];
 
 const gridAddFields: DataGridEditField[] = [
     {
@@ -126,7 +66,7 @@ export const AdminNextNobelsCardSettings: TableGridSettings = {
     defaultSorts: gridDefaultSorts,
     addFields: gridAddFields,
     editFields: gridAddFields,
-    addNewLabel: "Thêm mới",
-    deleteSelectedsLabel: "Xóa Thẻ cào NextNobels đã chọn",
-    updateLabel: "Cập nhật Thẻ cào NextNobels",
+    addNewLabel: gridAddNewLabel,
+    deleteSelectedsLabel: gridDeleteSelectedsLabel,
+    updateLabel: gridUpdateLabel,
 }
--- src/api/settings/OrderCardSettings.tsx
+++ src/api/settings/OrderCardSettings.tsx
@@ -1,94 +1,45 @@
-/**
- *
- * -- nextnobels.order_card definition
-
-CREATE TABLE `order_card` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cardId` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `amount` double NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `software` tinyint(4) NOT NULL DEFAULT 1,
-  `site` tinyint(4) NOT NULL DEFAULT 1,
-  `class` tinyint(4) NOT NULL,
-  `note` text NOT NULL,
-  `coupon` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3084 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
- */
-import { DataGridColumn, DataGridColumnActionType, DataGridColumnType, DataGridFilterColumn, DataGridFilterColumnType, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridTableJoin } from "@/components/grid/DataGrid";
+import { DataGridColumn, DataGridColumnType, DataGridColumns, DataGridFilterColumn, DataGridFilterColumns, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridSortOptions, DataGridTableJoin } from "@/components/grid/DataGrid";
 import { DataGridEditField, DataGridEditFieldType } from "@/components/grid/DataGridEdit";
 import { TableGridSettings } from "@/components/grid/TableGrid";
 
 const gridTitle: string = "Quản lý Đăng ký Mua thẻ";
+const gridAddNewLabel: string = "Thêm đăng ký Mua thẻ";
+const gridUpdateLabel: string = "Cập nhật đăng ký Mua thẻ";
+const gridDeleteSelectedsLabel: string = "Xóa các đăng ký Mua thẻ đã chọn";
 const gridTable: string = "order_card";
 const gridJoins: DataGridTableJoin[] = [];
 const gridSearchFields: string[] = ["id", "fullname", "address", "phone", "coupon"];
 const gridFields: string[] = ["id", "quantity", "date", "fullname", "address", "phone", "amount", "status", "class", "note", "coupon"];
 
 const gridColumns: DataGridColumn[] = [
-    { index: "id", label: "ID", width: "1%" },
+    DataGridColumns.id,
     { index: "quantity", label: "Số lượng", type: DataGridColumnType.NUMBER },
     { index: "date", label: "Ngày đăng ký", type: DataGridColumnType.DATE },
-    {index: "fullname", label: "Họ tên" },
+    { index: "fullname", label: "Họ tên" },
     { index: "address", label: "Địa chỉ" },
     { index: "phone", label: "Số điện thoại" },
     { index: "amount", label: "Số tiền", type: DataGridColumnType.CURRENCY },
     { index: "class", label: "Lớp", type: DataGridColumnType.TEXT },
     { index: "note", label: "Ghi chú" },
     { index: "coupon", label: "Coupon" },
-    {
-        index: "status", type: DataGridColumnType.STATUS, label: "Trạng thái", map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        hideLabel: true,
-        statusToggable: true
-    },
-    { index: "editAction", label: "Sửa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.EDIT, width: "1%" },
-    { index: "deleteAction", label: "Xóa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.DELETE, width: "1%" }
+    DataGridColumns.status,
+    DataGridColumns.editAction,
+    DataGridColumns.deleteAction,
 ];
 
-const gridPagination: DataGridPagination = {
-    currentPage: 1,
-    pageSize: 50
-};
+const gridPagination: DataGridPagination = { currentPage: 1, pageSize: 50 };
 
 const gridFilters: DataGridFilterColumn[] = [
-    { index: "id", label: "ID", type: DataGridFilterColumnType.TEXT },
-    {
-        index: "status", label: "Trạng thái", type: DataGridFilterColumnType.STATUS, map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        comparisonOperator: "equal"
-    },
+    DataGridFilterColumns.id,
+    DataGridFilterColumns.status,
 ];
 
 const gridSortOptions: DataGridSortOption[] = [
-    {
-        index: "idAsc",
-        label: "ID tăng",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.DESCENDING },
-        ]
-    },
-    {
-        index: "idDesc",
-        label: "ID giảm",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.ASCENDING },
-        ]
-    }
+    DataGridSortOptions.idAsc,
+    DataGridSortOptions.idDesc,
 ];
 
-const gridDefaultSorts: DataGridSort[] = [
-    { index: "id", direction: DataGridSortDirection.DESCENDING }
-];
+const gridDefaultSorts: DataGridSort[] = [{ index: "id", direction: DataGridSortDirection.DESCENDING }];
 
 const gridAddFields: DataGridEditField[] = [
     {
@@ -113,7 +64,7 @@ export const AdminOrderCardSettings: TableGridSettings = {
     defaultSorts: gridDefaultSorts,
     addFields: gridAddFields,
     editFields: gridAddFields,
-    addNewLabel: "Thêm mới",
-    deleteSelectedsLabel: "Xóa Đăng ký Mua thẻ đã chọn",
-    updateLabel: "Cập nhật Đăng ký Mua thẻ",
+    addNewLabel: gridAddNewLabel,
+    deleteSelectedsLabel: gridDeleteSelectedsLabel,
+    updateLabel: gridUpdateLabel,
 }
