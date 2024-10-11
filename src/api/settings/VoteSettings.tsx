a8f4ff6f70240c2076ca93c715db1d6c35b23234
Kien <kientrungle2001@gmail.com>
2024-10-11 19:04:40 +0700

    settings

--- src/api/settings/VoteSettings.tsx
+++ src/api/settings/VoteSettings.tsx
@@ -1,79 +1,39 @@
-/**
- * -- nextnobels.vote definition
-
-CREATE TABLE `vote` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `content` text NOT NULL,
-  `created` datetime NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
- * 
- */
-import { DataGridColumn, DataGridColumnActionType, DataGridColumnType, DataGridFilterColumn, DataGridFilterColumnType, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridTableJoin } from "@/components/grid/DataGrid";
+import { DataGridColumn, DataGridColumnActionType, DataGridColumns, DataGridColumnType, DataGridFilterColumn, DataGridFilterColumns, DataGridFilterColumnType, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridSortOptions, DataGridTableJoin } from "@/components/grid/DataGrid";
 import { DataGridEditField, DataGridEditFieldType } from "@/components/grid/DataGridEdit";
 import { TableGridSettings } from "@/components/grid/TableGrid";
 
 const gridTitle: string = "Quản lý Phản hồi";
 const gridTable: string = "vote";
+const gridAddNewLabel: string = "Thêm mới";
+const gridUpdateLabel: string = "Cập nhật Phản hồi";
+const gridDeleteSelectedsLabel: string = "Xóa các Phản hồi đã chọn";
 const gridJoins: DataGridTableJoin[] = [];
 const gridSearchFields: string[] = ["id", "content", "username"];
 const gridFields: string[] = ["id", "content", "username", "created", "status"];
 
 const gridColumns: DataGridColumn[] = [
-    { index: "id", label: "ID", width: "1%" },
+    DataGridColumns.id,
     { index: "content", label: "Nội dung" },
     { index: "username", label: "Tên đăng nhập" },
     { index: "created", label: "Ngày tạo", type: DataGridColumnType.DATE },
-    {
-        index: "status", type: DataGridColumnType.STATUS, label: "Trạng thái", map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        statusToggable: true
-    },
-    { index: "editAction", label: "Sửa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.EDIT, width: "1%" },
-    { index: "deleteAction", label: "Xóa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.DELETE, width: "1%" }
+    DataGridColumns.status,
+    DataGridColumns.editAction,
+    DataGridColumns.deleteAction,
 ];
 
-const gridPagination: DataGridPagination = {
-    currentPage: 1,
-    pageSize: 200
-};
+const gridPagination: DataGridPagination = { currentPage: 1, pageSize: 200 };
 
 const gridFilters: DataGridFilterColumn[] = [
-    { index: "id", label: "ID", type: DataGridFilterColumnType.TEXT },
-    {
-        index: "status", label: "Trạng thái", type: DataGridFilterColumnType.STATUS, map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        comparisonOperator: "equal"
-    },
+    DataGridFilterColumns.id,
+    DataGridFilterColumns.status,
 ];
 
 const gridSortOptions: DataGridSortOption[] = [
-    {
-        index: "idAsc",
-        label: "ID Phản hồi tăng",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.DESCENDING },
-        ]
-    },
-    {
-        index: "idDesc",
-        label: "ID Phản hồi giảm",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.ASCENDING },
-        ]
-    }
+    DataGridSortOptions.idAsc,
+    DataGridSortOptions.idDesc,
 ];
 
-const gridDefaultSorts: DataGridSort[] = [
-    { index: "id", direction: DataGridSortDirection.DESCENDING }
-];
+const gridDefaultSorts: DataGridSort[] = [{ index: "id", direction: DataGridSortDirection.DESCENDING }];
 
 const gridAddFields: DataGridEditField[] = [
 ];
@@ -92,8 +52,7 @@ export const AdminVoteSettings: TableGridSettings = {
     defaultSorts: gridDefaultSorts,
     addFields: gridAddFields,
     editFields: gridAddFields,
-    addNewLabel: "Thêm mới",
-    deleteSelectedsLabel: "Xóa Phản hồi đã chọn",
-    updateLabel: "Cập nhật Phản hồi",
-    treeMode: true,
+    addNewLabel: gridAddNewLabel,
+    deleteSelectedsLabel: gridDeleteSelectedsLabel,
+    updateLabel: gridUpdateLabel,
 }
--- src/api/settings/WalletSettings.tsx
+++ src/api/settings/WalletSettings.tsx
@@ -1,81 +1,38 @@
-/**
- * -- nextnobels.wallets definition
-
-CREATE TABLE `wallets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `amount` double NOT NULL,
-  `creatorId` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modifiedId` int(11) NOT NULL,
-  `modified` datetime NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=597 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
- * 
- */
-import { DataGridColumn, DataGridColumnActionType, DataGridColumnType, DataGridFilterColumn, DataGridFilterColumnType, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridTableJoin } from "@/components/grid/DataGrid";
+import { DataGridColumn, DataGridColumnActionType, DataGridColumns, DataGridColumnType, DataGridFilterColumn, DataGridFilterColumns, DataGridFilterColumnType, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridSortOptions, DataGridTableJoin } from "@/components/grid/DataGrid";
 import { DataGridEditField, DataGridEditFieldType } from "@/components/grid/DataGridEdit";
 import { TableGridSettings } from "@/components/grid/TableGrid";
 
 const gridTitle: string = "Quản lý Ví điện tử";
 const gridTable: string = "wallets";
+const gridAddNewLabel: string = "Thêm mới";
+const gridUpdateLabel: string = "Cập nhật Ví điện tử";
+const gridDeleteSelectedsLabel: string = "Xóa các Ví điện tử đã chọn";
 const gridJoins: DataGridTableJoin[] = [];
 const gridSearchFields: string[] = ["id", "username"];
 const gridFields: string[] = ["id", "username", "amount", "status"];
 
 const gridColumns: DataGridColumn[] = [
-    { index: "id", label: "ID", width: "1%" },
+    DataGridColumns.id,
     { index: "username", label: "Tên đăng nhập" },
     { index: "amount", label: "Số dư", type: DataGridColumnType.CURRENCY },
-    {
-        index: "status", type: DataGridColumnType.STATUS, label: "Trạng thái", map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        statusToggable: true
-    },
-    { index: "editAction", label: "Sửa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.EDIT, width: "1%" },
-    { index: "deleteAction", label: "Xóa", type: DataGridColumnType.ACTIONS, actionType: DataGridColumnActionType.DELETE, width: "1%" }
+    DataGridColumns.status,
+    DataGridColumns.editAction,
+    DataGridColumns.deleteAction
 ];
 
-const gridPagination: DataGridPagination = {
-    currentPage: 1,
-    pageSize: 200
-};
+const gridPagination: DataGridPagination = { currentPage: 1, pageSize: 200 };
 
 const gridFilters: DataGridFilterColumn[] = [
-    { index: "id", label: "ID", type: DataGridFilterColumnType.TEXT },
-    {
-        index: "status", label: "Trạng thái", type: DataGridFilterColumnType.STATUS, map: {
-            0: 'Chưa kích hoạt',
-            1: 'Đã kích hoạt'
-        },
-        comparisonOperator: "equal"
-    },
+    DataGridFilterColumns.id,
+    DataGridFilterColumns.status,
 ];
 
 const gridSortOptions: DataGridSortOption[] = [
-    {
-        index: "idAsc",
-        label: "ID Ví điện tử tăng",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.DESCENDING },
-        ]
-    },
-    {
-        index: "idDesc",
-        label: "ID Ví điện tử giảm",
-        sorts: [
-            { index: "id", direction: DataGridSortDirection.ASCENDING },
-        ]
-    }
+    DataGridSortOptions.idAsc,
+    DataGridSortOptions.idDesc,
 ];
 
-const gridDefaultSorts: DataGridSort[] = [
-    { index: "id", direction: DataGridSortDirection.DESCENDING }
-];
+const gridDefaultSorts: DataGridSort[] = [{ index: "id", direction: DataGridSortDirection.DESCENDING }];
 
 const gridAddFields: DataGridEditField[] = [
 ];
@@ -94,8 +51,7 @@ export const AdminWalletSettings: TableGridSettings = {
     defaultSorts: gridDefaultSorts,
     addFields: gridAddFields,
     editFields: gridAddFields,
-    addNewLabel: "Thêm mới",
-    deleteSelectedsLabel: "Xóa Ví điện tử đã chọn",
-    updateLabel: "Cập nhật Ví điện tử",
-    treeMode: true,
+    addNewLabel: gridAddNewLabel,
+    deleteSelectedsLabel: gridDeleteSelectedsLabel,
+    updateLabel: gridUpdateLabel,
 }
