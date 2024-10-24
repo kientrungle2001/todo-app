609c8718e4915113fa23b62f6a7fa5708244d3b0
Kien <kientrungle2001@gmail.com>
2024-10-24 18:44:10 +0700

    student

--- src/pages/TableUsers.tsx
+++ src/pages/TableUsers.tsx
@@ -1,9 +1,9 @@
-import { StudentSettings } from "@/api/settings/StudentSettings";
+import { FullLookStudentSettings } from "@/api/settings/FullLookStudentSettings";
 import { TableGrid } from "@/components/grid/TableGrid";
 import React from "react";
 
 export default function TableUsers(): React.ReactElement {
     return <>
-        <TableGrid controller="admin_student" settings={StudentSettings} />
+        <TableGrid controller="admin_student" settings={FullLookStudentSettings} />
     </>
 }
