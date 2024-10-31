e03f8832f1df7d3b0b29b3c031e0ab68c422cbda
Kien <kientrungle2001@gmail.com>
2024-10-31 17:06:20 +0700

    news

--- ci313/application/models/Route_model.php
+++ ci313/application/models/Route_model.php
@@ -6,17 +6,21 @@ class route_model extends CI_Model {
         $this->db->where('alias', $alias);
         $category = $this->db->get('categories')->row();
         if ($category) {
+            $newsList = $this->db->where('categoryId', $category->id)->get('news')->result_array();
             return array(
                 'type' => 'category',
-                'category' => $category
+                'category' => $category,
+                'newsList' => $newsList
             );
         }
         $this->db->where('alias', $alias);
         $news = $this->db->get('news')->row();
         if ($news) {
+            $category = $this->db->where('id', $news->categoryId)->get('categories')->row();
             return array(
                 'type' => 'news',
-                'news' => $news
+                'news' => $news,
+                'category' => $category
             );
         }
         return null;
