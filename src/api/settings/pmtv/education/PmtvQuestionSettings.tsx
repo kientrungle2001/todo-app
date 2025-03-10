38f55d83f4f4c1e6d81592208e8dfabb40172c09
Kien <kientrungle2001@gmail.com>
2025-03-10 22:55:02 +0700

    questions

--- src/api/settings/pmtv/education/PmtvQuestionSettings.tsx
+++ src/api/settings/pmtv/education/PmtvQuestionSettings.tsx
@@ -1,25 +1,21 @@
-import { DataGridColumn, DataGridFilterColumn, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridSortOptions as sortOptions, DataGridTableJoin } from "@/components/grid/DataGridColumnTypes";
+import { DataGridColumn as Column, DataGridFilterColumn as FilterField, DataGridPagination as Pagination, DataGridSort as Sort, DataGridSortDirection as SortDirection, DataGridSortOption as SortOption, DataGridSortOptions as sortOptions } from "@/components/grid/DataGridColumnTypes";
 import { DataGridColumns as columns } from "@/components/grid/DataGridColumns";
 import { DataGridEditFields as editFields } from "@/components/grid/DataGridEditFields";
-import { DataGridEditField } from "@/components/grid/DataGridEditTypes";
+import { DataGridEditField as EditField } from "@/components/grid/DataGridEditTypes";
 import { DataGridFilterColumns as filterFields } from "@/components/grid/DataGridFilterColumns";
-import { TableGridSettings } from "@/components/grid/TableGrid";
+import { TableGridSettings as Settings } from "@/components/grid/TableGrid";
 
 const gridTitle: string = "Quản lý Câu hỏi";
 const gridAddNewLabel: string = "Thêm mới Câu hỏi";
 const gridUpdateLabel: string = "Cập nhật Câu hỏi";
 const gridDeleteSelectedsLabel: string = "Xóa Câu hỏi đã chọn";
 const gridTable: string = "questions";
-const gridJoins: DataGridTableJoin[] = [];
 const gridSearchFields: string[] = ["id", "name"];
 const gridFields: string[] = ["id", "name", "ordering", "status", "trial", "categoryIds", "courseId", "courseResourceId", "questionType"];
-
-const gridColumns: DataGridColumn[] = [
+const formatQuestionLink = (value: any, item: any) => `/Table/admin_question2/${item.id}/` + (item.questionType == 4 ? 'edit' : 'detail');
+const gridColumns: Column[] = [
     columns.id,
-    {
-        ...columns.question_content,
-        linkFormat: (value: any, item: any) => `/Table/admin_question2/${item.id}/` + (item.questionType == 4 ? 'edit' : 'detail'),
-    },
+    { ...columns.question_content, linkFormat: formatQuestionLink },
     columns.categoryIds,
     columns.courseId,
     columns.courseResourceId,
@@ -30,23 +26,23 @@ const gridColumns: DataGridColumn[] = [
     columns.deleteAction
 ];
 
-const gridPagination: DataGridPagination = { currentPage: 1, pageSize: 50 };
+const gridPagination: Pagination = { currentPage: 1, pageSize: 50 };
 
-const gridFilters: DataGridFilterColumn[] = [
+const gridFilters: FilterField[] = [
     filterFields.categoryIds,
     { ...filterFields.courseId, tableCondition: (item) => "categoryId = '" + item.categoryIds + "'" },
     { ...filterFields.courseResourceId, tableCondition: (item) => "courseId = '" + item.courseId + "'" },
     filterFields.status,
 ];
 
-const gridSortOptions: DataGridSortOption[] = [
+const gridSortOptions: SortOption[] = [
     sortOptions.orderingAsc,
     sortOptions.orderingDesc,
 ];
 
-const gridDefaultSorts: DataGridSort[] = [{ index: "id", direction: DataGridSortDirection.DESCENDING }];
+const gridDefaultSorts: Sort[] = [{ index: "id", direction: SortDirection.DESCENDING }];
 
-const gridAddFields: DataGridEditField[] = [
+const gridAddFields: EditField[] = [
     { ...editFields.question_content, tabGroup: "2name" },
     { ...editFields.explaination, tabGroup: "3explaination" },
     { ...editFields.classes, tabGroup: "0classification" },
@@ -57,10 +53,9 @@ const gridAddFields: DataGridEditField[] = [
     editFields.status,
 ];
 
-export const PmtvAdminQuestionSettings: TableGridSettings = {
+export const PmtvAdminQuestionSettings: Settings = {
     title: gridTitle,
     table: gridTable,
-    joins: gridJoins,
     fields: gridFields,
     searchFields: gridSearchFields,
     pagination: gridPagination,
