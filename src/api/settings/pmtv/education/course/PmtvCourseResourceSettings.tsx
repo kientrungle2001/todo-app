d09dd3499690ee91ad369d7d26b1b7cd9de1213d
Kien <kientrungle2001@gmail.com>
2025-03-10 23:42:18 +0700

    resource

--- src/api/settings/pmtv/education/course/PmtvCourseResourceSettings.tsx
+++ src/api/settings/pmtv/education/course/PmtvCourseResourceSettings.tsx
@@ -1,16 +1,16 @@
-import { DataGridColumn, DataGridFilterColumn, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridSortOptions, DataGridTableJoin } from "@/components/grid/DataGridColumnTypes";
-import { DataGridColumns } from "@/components/grid/DataGridColumns";
-import { DataGridEditFields } from "@/components/grid/DataGridEditFields";
-import { DataGridEditField, DataGridEditFieldType } from "@/components/grid/DataGridEditTypes";
-import { DataGridFilterColumns } from "@/components/grid/DataGridFilterColumns";
-import { TableGridSettings } from "@/components/grid/TableGrid";
+import { DataGridColumn as Column, DataGridFilterColumn as FilterField, DataGridPagination as Pagination, DataGridSort as Sort, DataGridSortDirection as SortDirection, DataGridSortOption as SortOption, DataGridSortOptions as SortOptions, DataGridTableJoin as Join } from "@/components/grid/DataGridColumnTypes";
+import { DataGridColumns as columns } from "@/components/grid/DataGridColumns";
+import { DataGridEditFields as editFields } from "@/components/grid/DataGridEditFields";
+import { DataGridEditField as EditField, DataGridEditFieldType as EditType } from "@/components/grid/DataGridEditTypes";
+import { DataGridFilterColumns as filterFields } from "@/components/grid/DataGridFilterColumns";
+import { TableGridSettings as Settings } from "@/components/grid/TableGrid";
 
 const gridTitle: string = "Quản lý Tài nguyên Khóa học";
 const gridAddNewLabel: string = "Thêm Tài nguyên Khóa học";
 const gridUpdateLabel: string = "Cập nhật Tài nguyên Khóa học";
 const gridDeleteSelectedsLabel: string = "Xóa các Tài nguyên Khóa học đã chọn";
 const gridTable: string = "courses_resources";
-const gridJoins: DataGridTableJoin[] = [
+const gridJoins: Join[] = [
     {
         table: 'courses',
         alias: 'c',
@@ -21,59 +21,59 @@ const gridJoins: DataGridTableJoin[] = [
 const gridSearchFields: string[] = ["id", "name"];
 const gridFields: string[] = ["id", "parent", "name", "alias", "courseId", "ordering", "status", "image", "type", "c.categoryId as categoryIds", '1.0 as questionType'];
 
-const gridColumns: DataGridColumn[] = [
-    DataGridColumns.id,
+const gridColumns: Column[] = [
+    columns.id,
     {
         index: "name", label: "Tên Tài nguyên Khóa học",
         linkFormat: (value: any, item: any) => `/Table/admin_course_resource/${item.id}/detail`,
         treeMode: true
     },
-    DataGridColumns.alias,
-    DataGridColumns.courseId,
-    DataGridColumns.ordering,
-    DataGridColumns.status,
-    { ...DataGridColumns.addChildAction, actionAddChildParentFields: ['courseId', 'status'] },
-    { ...DataGridColumns.addChildAction, index: 'addQuestion', label: 'Thêm câu hỏi', actionAddChildController: 'admin_question2', actionAddChildParentField: 'courseResourceId', actionAddChildParentFields: ['courseId', 'status', 'categoryIds', 'questionType'] },
-    DataGridColumns.editAction,
-    DataGridColumns.deleteAction
+    columns.alias,
+    columns.courseId,
+    columns.ordering,
+    columns.status,
+    { ...columns.addChildAction, actionAddChildParentFields: ['courseId', 'status'] },
+    { ...columns.addChildAction, index: 'addQuestion', label: 'Thêm câu hỏi', actionAddChildController: 'admin_question2', actionAddChildParentField: 'courseResourceId', actionAddChildParentFields: ['courseId', 'status', 'categoryIds', 'questionType'] },
+    columns.editAction,
+    columns.deleteAction
 ];
 
-const gridPagination: DataGridPagination = { currentPage: 1, pageSize: 5000 };
+const gridPagination: Pagination = { currentPage: 1, pageSize: 5000 };
 
-const gridFilters: DataGridFilterColumn[] = [
-    DataGridFilterColumns.id,
-    DataGridFilterColumns.courseId,
-    DataGridFilterColumns.status
+const gridFilters: FilterField[] = [
+    filterFields.id,
+    filterFields.courseId,
+    filterFields.status
 ];
 
-const gridSortOptions: DataGridSortOption[] = [
-    { ...DataGridSortOptions.nameAsc, label: "Tên Tài nguyên Khóa học tăng" },
-    { ...DataGridSortOptions.nameDesc, label: "Tên Tài nguyên Khóa học giảm" },
+const gridSortOptions: SortOption[] = [
+    { ...SortOptions.nameAsc, label: "Tên Tài nguyên Khóa học tăng" },
+    { ...SortOptions.nameDesc, label: "Tên Tài nguyên Khóa học giảm" },
 ];
 
-const gridDefaultSorts: DataGridSort[] = [{ index: "ordering", direction: DataGridSortDirection.ASCENDING }];
+const gridDefaultSorts: Sort[] = [{ index: "ordering", direction: SortDirection.ASCENDING }];
 
-const gridAddFields: DataGridEditField[] = [
-    DataGridEditFields.courseId,
+const gridAddFields: EditField[] = [
+    editFields.courseId,
     {
-        index: "parent", label: "Mục cha", type: DataGridEditFieldType.SELECT, size: 8,
+        index: "parent", label: "Mục cha", type: EditType.SELECT, size: 8,
         table: "courses_resources", valueField: "id", labelField: "name", treeMode: true,
         tableCondition: (item) => "courseId = '" + item.courseId + "' and id <> '" + (item.id ?? 0) + "'",
     },
-    { ...DataGridEditFields.name, label: "Tên Tài nguyên Khóa học", size: 12 },
-    { ...DataGridEditFields.alias, size: 12 },
-    DataGridEditFields.type,
-    DataGridEditFields.image,
-    DataGridEditFields.brief,
-    DataGridEditFields.content,
-    DataGridEditFields.hardiness,
-    DataGridEditFields.totalMinutes,
-    DataGridEditFields.trial,
-    DataGridEditFields.status,
+    { ...editFields.name, label: "Tên Tài nguyên Khóa học", size: 12 },
+    { ...editFields.alias, size: 12 },
+    editFields.type,
+    editFields.image,
+    editFields.brief,
+    editFields.content,
+    editFields.hardiness,
+    editFields.totalMinutes,
+    editFields.trial,
+    editFields.status,
 ];
 
 
-export const PmtvAdminCourseResourceSettings: TableGridSettings = {
+export const PmtvAdminCourseResourceSettings: Settings = {
     title: gridTitle,
     table: gridTable,
     joins: gridJoins,
