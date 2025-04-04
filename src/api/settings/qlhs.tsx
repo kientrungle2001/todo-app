e0c99d82b140a3ced478bb89be2c63a29a98c18b
Kien <kientrungle2001@gmail.com>
2025-04-05 01:41:16 +0700

    subject

--- src/api/settings/qlhs.tsx
+++ src/api/settings/qlhs.tsx
@@ -1,6 +1,7 @@
 import { TableGridSettings } from "@/components/grid/TableGrid";
 import { QlhsStudentSettings } from "./qlhs/QlhsStudentSettings";
 import { QlhsClassSettings } from "./qlhs/QlhsClassSettings";
+import { QlhsSubjectSettings } from "./qlhs/QlhsSubjectSettings";
 
 export const getSettingsByControllerForQLHS = (controller: string, hostname: string = 'localhost'): TableGridSettings | null => {
     if (controller === 'student') {
@@ -8,6 +9,9 @@ export const getSettingsByControllerForQLHS = (controller: string, hostname: str
     }
     if (controller === 'class') {
         return QlhsClassSettings;
+    }
+    if (controller === 'subject') {
+        return QlhsSubjectSettings;
     }
     return null;
 }
---
+++ src/api/settings/qlhs/QlhsSubjectSettings.tsx
@@ -0,0 +1,87 @@
+import { DataGridColumn, DataGridFilterColumn, DataGridPagination, DataGridSort, DataGridSortDirection, DataGridSortOption, DataGridSortOptions, DataGridTableJoin } from "@/components/grid/DataGridColumnTypes";
+import { DataGridColumns } from "@/components/grid/DataGridColumns";
+import { DataGridEditField, DataGridEditFieldType } from "@/components/grid/DataGridEditTypes";
+import { DataGridFilterColumns } from "@/components/grid/DataGridFilterColumns";
+import { TableGridDetail, TableGridDetailType, TableGridSettings } from "@/components/grid/TableGrid";
+import { QlhsClassSettings } from "./QlhsClassSettings";
+
+const gridTitle: string = "Quản lý Môn học";
+const gridAddNewLabel: string = "Thêm Môn học";
+const gridUpdateLabel: string = "Cập nhật Môn học";
+const gridDeleteSelectedsLabel: string = "Xóa các Môn học đã chọn";
+const gridTable: string = "subject";
+const gridJoins: DataGridTableJoin[] = [];
+const gridSearchFields: string[] = ["id", "name"];
+const gridFields: string[] = ["id", "name", "status"];
+
+const gridColumns: DataGridColumn[] = [
+    DataGridColumns.id,
+    {
+        index: "name", label: "Tên Lớp", linkFormat: (name: any, item: any): string => {
+            return '/Table/subject/' + item.id + '/detail';
+        }
+    },
+    DataGridColumns.status,
+    DataGridColumns.editAction,
+    DataGridColumns.deleteAction,
+];
+
+const gridPagination: DataGridPagination = { currentPage: 1, pageSize: 20 };
+
+const gridFilters: DataGridFilterColumn[] = [
+    DataGridFilterColumns.id,
+    DataGridFilterColumns.status,
+];
+
+const gridSortOptions: DataGridSortOption[] = [
+    DataGridSortOptions.idAsc,
+    DataGridSortOptions.idDesc,
+];
+
+const gridDefaultSorts: DataGridSort[] = [{ index: "id", direction: DataGridSortDirection.DESCENDING }];
+
+const gridAddFields: DataGridEditField[] = [
+    {
+        index: 'name',
+        label: 'Tên Môn học',
+        type: DataGridEditFieldType.TEXT,
+        tabGroup: '0info',
+        size: 6
+    },
+];
+
+const gridDetails: TableGridDetail[] = [
+    {
+        type: TableGridDetailType.DETAIL,
+        fields: [
+            { ...DataGridColumns.id, size: 4 },
+            { index: "name", label: "Tên Lớp", size: 4 },
+            { ...DataGridColumns.status, size: 4 }
+        ]
+    },
+    {
+        type: TableGridDetailType.GRID,
+        controller: 'class',
+        referenceField: 'subjectId',
+        referenceType: 'equal',
+        settings: QlhsClassSettings
+    }
+];
+export const QlhsSubjectSettings: TableGridSettings = {
+    title: gridTitle,
+    table: gridTable,
+    joins: gridJoins,
+    fields: gridFields,
+    searchFields: gridSearchFields,
+    pagination: gridPagination,
+    columns: gridColumns,
+    filters: gridFilters,
+    sortOptions: gridSortOptions,
+    defaultSorts: gridDefaultSorts,
+    addFields: gridAddFields,
+    editFields: gridAddFields,
+    addNewLabel: gridAddNewLabel,
+    deleteSelectedsLabel: gridDeleteSelectedsLabel,
+    updateLabel: gridUpdateLabel,
+    details: gridDetails
+}
