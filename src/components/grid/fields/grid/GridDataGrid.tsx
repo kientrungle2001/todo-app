70dcea2da7c5fe6e114b4c56a6aef9d8984703a0
Kien <kientrungle2001@gmail.com>
2025-05-17 23:56:16 +0700

    grid

--- src/components/grid/fields/grid/GridDataGrid.tsx
+++ src/components/grid/fields/grid/GridDataGrid.tsx
@@ -1,32 +1,21 @@
-import { Card, Col, Container, Row, Table } from "react-bootstrap";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-
-import {
-    DataGridColumn as Column, DataGridFilterColumn,
-    DataGridPagination,
-    DataGridSort,
-    DataGridSortOption
-} from "../../DataGridColumnTypes";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { FiltersGridCard } from "../../filters/FilterGridCard";
-import { PaginationGrid } from "../../PaginationGrid";
-import { renderColumn } from "../../columns/renderColumn";
-import { GridDataGridHead } from "./GridDataGridHead";
+import { GridTableWrapper } from "./GridTableWrapper";
 
-interface GridDataGridProps {
+interface Props {
     title: string;
     table: string;
     software?: number;
     site?: number;
-    columns: Column[];
+    columns: any[];
     items: any[];
     totalItems: number;
-    filters?: DataGridFilterColumn[];
-    defaultSorts?: DataGridSort[];
-    sortOptions?: DataGridSortOption[];
-    pagination?: DataGridPagination;
-    sorts: DataGridSort[];
-    setSorts: (sorts: DataGridSort[]) => void;
+    filters?: any[];
+    defaultSorts?: any[];
+    sortOptions?: any[];
+    pagination?: any;
+    sorts: any[];
+    setSorts: (sorts: any[]) => void;
     searchText?: string;
     setSearchText: (searchText: string) => void;
     filterData?: any;
@@ -39,70 +28,73 @@ interface GridDataGridProps {
     setSelectedItem: (selectedItem: any) => void;
 }
 
-const GridDataGrid: React.FC<GridDataGridProps> = ({ title, table, software, site, columns = [], filters = [], defaultSorts, sortOptions, items = [], pagination, setCurrentPage, setPageSize, searchText, setSearchText, filterData, setFilterData, sorts, setSorts, totalItems, sortData, setSortData, selectedItem, setSelectedItem }) => {
+const GridDataGrid: React.FC<Props> = ({
+    title,
+    table,
+    software,
+    site,
+    columns,
+    items,
+    totalItems,
+    filters = [],
+    defaultSorts,
+    sortOptions,
+    pagination,
+    sorts,
+    setSorts,
+    searchText,
+    setSearchText,
+    filterData,
+    setFilterData,
+    setCurrentPage,
+    setPageSize,
+    sortData,
+    setSortData,
+    selectedItem,
+    setSelectedItem,
+}) => {
+    return (
+        <Container fluid className="mb-0 mt-0">
+            <Row className="g-0">
+                <Col sm={12} md={12} lg={12}>
+                    <FiltersGridCard
+                        filters={filters}
+                        sortOptions={sortOptions}
+                        filterData={filterData}
+                        setFilterData={setFilterData}
+                        searchText={searchText}
+                        setSearchText={setSearchText}
+                        sorts={sorts}
+                        setSorts={setSorts}
+                        defaultSorts={defaultSorts}
+                    />
+                </Col>
+                <Col sm={12} md={12} lg={12}>
+                    <Card className="border-0">
+                        <Card.Body className="border-0 pt-0">
+                            <Card.Title className="d-flex justify-content-between align-items-center">
+                                <span>{title}</span>
+                            </Card.Title>
 
-    const toggleCheckedItem = (item: any) => {
-        if (selectedItem === item) {
-            setSelectedItem(null);
-        } else {
-            setSelectedItem(item);
-        }
-    }
-
-    return <Container fluid className="mb-0 mt-0">
-        <Row className="g-0">
-            <Col sm={12} md={12} lg={12}>
-                <FiltersGridCard filters={filters} sortOptions={sortOptions} filterData={filterData} setFilterData={setFilterData} searchText={searchText} setSearchText={setSearchText} sorts={sorts} setSorts={setSorts} defaultSorts={defaultSorts} />
-            </Col>
-            <Col sm={12} md={12} lg={12}>
-                <Card className="border-0">
-                    <Card.Body className="border-0 pt-0">
-                        <Card.Title className="d-flex justify-content-between align-items-center">
-                            {/* Title on the left */}
-                            <span>{title}</span>
-                        </Card.Title>
-
-                        <div className="table-responsive">
-                            <Table size="sm" striped hover>
-                                <thead>
-                                    <tr>
-                                        <GridDataGridHead table={table} columns={columns} sortData={sortData} setSortData={setSortData} />
-                                    </tr>
-                                    <tr>
-                                        <td colSpan={columns.length + 1}>
-                                            <PaginationGrid totalItems={totalItems} setCurrentPage={setCurrentPage} setPageSize={setPageSize} pagination={pagination} />
-                                        </td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {items.length ? items.map((item, index) =>
-                                        <tr key={index} onClick={() => toggleCheckedItem(item)} style={{ "cursor": "pointer" }}
-                                            className={selectedItem === item ? 'table-primary' : ''}>
-
-                                            {columns.map(column => (
-                                                <td key={column.index} style={{ width: column.width, whiteSpace: (column.inputable) ? 'nowrap' : 'normal' }}>
-                                                    {renderColumn(column, item, table, {}, () => { }, () => { }, () => { }, () => { }, () => { })}
-                                                </td>
-                                            ))}
-                                        </tr>)
-                                        : <tr>
-                                            <td colSpan={columns.length + 1} className="text-center">No data available.</td>
-                                        </tr>}
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <td colSpan={columns.length + 1}>
-                                            <PaginationGrid totalItems={totalItems} setCurrentPage={setCurrentPage} setPageSize={setPageSize} pagination={pagination} />
-                                        </td>
-                                    </tr>
-                                </tfoot>
-                            </Table>
-                        </div>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
-    </Container>
-}
+                            <GridTableWrapper
+                                table={table}
+                                columns={columns}
+                                items={items}
+                                sortData={sortData}
+                                setSortData={setSortData}
+                                selectedItem={selectedItem}
+                                setSelectedItem={setSelectedItem}
+                                pagination={pagination}
+                                totalItems={totalItems}
+                                setCurrentPage={setCurrentPage}
+                                setPageSize={setPageSize}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default GridDataGrid;
---
+++ src/components/grid/fields/grid/GridTableWrapper.tsx
@@ -0,0 +1,105 @@
+import { Table } from "react-bootstrap";
+import { renderColumn } from "../../columns/renderColumn";
+import { GridDataGridHead } from "./GridDataGridHead";
+import { PaginationGrid } from "../../PaginationGrid";
+
+interface Props {
+    table: string;
+    columns: any[];
+    items: any[];
+    sortData: any;
+    setSortData: (data: any) => void;
+    selectedItem: any;
+    setSelectedItem: (item: any) => void;
+    pagination: any;
+    totalItems: number;
+    setCurrentPage: (page: number) => void;
+    setPageSize: (size: number) => void;
+}
+
+export const GridTableWrapper: React.FC<Props> = ({
+    table,
+    columns,
+    items,
+    sortData,
+    setSortData,
+    selectedItem,
+    setSelectedItem,
+    pagination,
+    totalItems,
+    setCurrentPage,
+    setPageSize,
+}) => {
+    const toggleCheckedItem = (item: any) => {
+        setSelectedItem(selectedItem === item ? null : item);
+    };
+
+    return (
+        <div className="table-responsive">
+            <Table size="sm" striped hover>
+                <thead>
+                    <tr>
+                        <GridDataGridHead
+                            table={table}
+                            columns={columns}
+                            sortData={sortData}
+                            setSortData={setSortData}
+                        />
+                    </tr>
+                    <tr>
+                        <td colSpan={columns.length + 1}>
+                            <PaginationGrid
+                                totalItems={totalItems}
+                                setCurrentPage={setCurrentPage}
+                                setPageSize={setPageSize}
+                                pagination={pagination}
+                            />
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.length ? (
+                        items.map((item, index) => (
+                            <tr
+                                key={index}
+                                onClick={() => toggleCheckedItem(item)}
+                                style={{ cursor: "pointer" }}
+                                className={selectedItem === item ? "table-primary" : ""}
+                            >
+                                {columns.map((column) => (
+                                    <td
+                                        key={column.index}
+                                        style={{
+                                            width: column.width,
+                                            whiteSpace: column.inputable ? "nowrap" : "normal",
+                                        }}
+                                    >
+                                        {renderColumn(column, item, table, {}, () => { }, () => { }, () => { }, () => { }, () => { })}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={columns.length + 1} className="text-center">
+                                No data available.
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={columns.length + 1}>
+                            <PaginationGrid
+                                totalItems={totalItems}
+                                setCurrentPage={setCurrentPage}
+                                setPageSize={setPageSize}
+                                pagination={pagination}
+                            />
+                        </td>
+                    </tr>
+                </tfoot>
+            </Table>
+        </div>
+    );
+};
